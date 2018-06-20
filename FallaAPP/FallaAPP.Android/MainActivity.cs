10aba2c8a68efb97aae4ef33ad388a083c645257
Antonio <alorenzoesparza@gmail.com>
2018-06-20 10:25:09 +0200

    Camara en Android

--- FallaAPP/FallaAPP.Android/MainActivity.cs
+++ FallaAPP/FallaAPP.Android/MainActivity.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
+using Plugin.Permissions;
 
 namespace FallaAPP.Droid
 {
-    [Activity(Label = "FallaAPP", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FallaAPP", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -21,6 +22,16 @@ namespace FallaAPP.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(
+            int requestCode, 
+            string[] permissions, 
+            [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(
+                requestCode, 
+                permissions, 
+                grantResults);
         }
     }
 }
